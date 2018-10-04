ca4b3be62856a019fad2c369fb40d49cc764bc2a
zaburo <zaburo90@gmail.com>
2018-10-04 15:02:03 +0100

    Update const.js
    location -> localhost

--- vote-app/client/src/components/const.js
+++ vote-app/client/src/components/const.js
@@ -1,5 +1,5 @@
 export const URL =  window.location.protocol+"//"+window.location.hostname;
 var api = URL+':8080';
-if(window.location.hostname!='location')
+if(window.location.hostname!='localhost')
   api = 'https://voteapi.builtwithblockchain.io';
 export const API=api;
